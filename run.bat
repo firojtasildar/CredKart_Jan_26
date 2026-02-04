2bc5923fe4d854e2b33cba18a2413c9e8abae488
firojtasildar <firojtasildar117@gmail.com>
2026-02-04 23:51:43 +0530

    edited

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 pytest -n=auto --html=Html_reports\my_chromereport_04_02_2026.html --browser chrome
 pytest -n=auto --html=Html_reports\my_chromereport_04_02_2026.html --browser firefox
+pytest -n=auto --html=Html_reports\my_chromereport_04_02_2026.html --browser edge
