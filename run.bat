0a7a4e262f56ae1cdb44ed3c499c733f6de62610
firojtasildar <firojtasildar117@gmail.com>
2026-02-05 11:20:59 +0530

    edited with run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-pytest -n=auto --html=Html_reports\my_chromereport_04_02_2026.html --browser chrome
-pytest -n=auto --html=Html_reports\my_chromereport_04_02_2026.html --browser firefox
-pytest -n=auto --html=Html_reports\my_chromereport_04_02_2026.html --browser edge
+pytest --html=Html_reports\my_chromereport_04_02_2026.html --browser chrome
+pytest --html=Html_reports\my_chromereport_04_02_2026.html --browser firefox
+pytest --html=Html_reports\my_chromereport_04_02_2026.html --browser edge
